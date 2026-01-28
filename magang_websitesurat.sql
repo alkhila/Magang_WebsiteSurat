0a998f1b5637c9adbf3799f033c0d9118018ae9f
Zahra <nafiahzahra15@gmail.com>
2026-01-28 09:32:41 +0700

    Add files via upload

--- magang_websitesurat.sql
+++ magang_websitesurat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 26, 2026 at 02:47 AM
+-- Generation Time: Jan 28, 2026 at 03:32 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -34,6 +34,27 @@ CREATE TABLE `pengendali` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengendali_sisipan`
+--
+
+CREATE TABLE `pengendali_sisipan` (
+  `no_urut` varchar(10) NOT NULL,
+  `klas` varchar(50) NOT NULL,
+  `plus` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pengendali_sisipan`
+--
+
+INSERT INTO `pengendali_sisipan` (`no_urut`, `klas`, `plus`, `created_at`) VALUES
+('1', '1000', 'Sub Bagian Umum dan Kepegawaian', '2026-01-28 02:23:08'),
+('2', '0000', 'Sub Bagian KPE', '2026-01-28 02:23:32');
+
 --
 -- Indexes for dumped tables
 --
@@ -42,6 +63,12 @@ CREATE TABLE `pengendali` (
 -- Indexes for table `pengendali`
 --
 ALTER TABLE `pengendali`
+  ADD PRIMARY KEY (`no_urut`);
+
+--
+-- Indexes for table `pengendali_sisipan`
+--
+ALTER TABLE `pengendali_sisipan`
   ADD PRIMARY KEY (`no_urut`);
 COMMIT;
 
