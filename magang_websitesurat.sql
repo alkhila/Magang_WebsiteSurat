6d3088db7c6e2b062dfa3727a56d15f651f6ce67
Alkhila <alkhilasyadza@gmail.com>
2026-02-10 14:47:04 +0700

    Tambahin tabel surat SPT

--- magang_websitesurat.sql
+++ magang_websitesurat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 06, 2026 at 08:22 AM
+-- Generation Time: Feb 10, 2026 at 08:46 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -59,10 +59,13 @@ CREATE TABLE `pengendali` (
 --
 
 INSERT INTO `pengendali` (`no_urut`, `klas`, `plus`, `tanggal_manual`, `created_at`) VALUES
-(4, '765', 'Sub Bagian Umum dan Kepegawaian', NULL, '2026-02-05 01:19:36'),
-(6, '765', 'Sub Bagian KPE', NULL, '2026-02-05 02:21:16'),
-(7, '123', 'Bidang Arsip', NULL, '2026-02-05 02:21:35'),
-(8, '999', 'Bidang Perpustakaan', NULL, '2026-02-06 06:58:49');
+(7, '123', 'Sub Bagian Umum dan Kepegawaian', NULL, '2026-02-05 02:21:35'),
+(9, '767', 'Sub Bagian Umum dan Kepegawaian', NULL, '2026-02-09 06:45:52'),
+(10, '999', 'Bidang Perpustakaan', NULL, '2026-02-09 07:37:40'),
+(11, '000.5.4.3', 'Bidang Perpustakaan', NULL, '2026-02-10 01:00:30'),
+(12, '9999.01', 'Bidang Perpustakaan', NULL, '2026-02-10 01:54:45'),
+(13, '999', 'Bidang Perpustakaan', NULL, '2026-02-10 03:54:09'),
+(14, '765', 'Sub Bagian Umum dan Kepegawaian', NULL, '2026-02-10 07:43:42');
 
 -- --------------------------------------------------------
 
@@ -85,7 +88,59 @@ CREATE TABLE `pengendali_sisipan` (
 INSERT INTO `pengendali_sisipan` (`no_urut`, `klas`, `plus`, `tanggal_manual`, `created_at`) VALUES
 ('000.5.1', 'tes jaaa', 'Bidang Arsip', NULL, '2026-02-04 02:26:09'),
 ('1.a', '123', 'Bidang PSP', '2026-02-02', '2026-02-05 06:52:03'),
-('4.a', '1000', 'Sub Bagian Umum dan Kepegawaian', '2026-02-06', '2026-02-06 06:58:08');
+('12.1', '999', 'Sub Bagian Umum dan Kepegawaian', '2026-02-10', '2026-02-10 07:11:53'),
+('4.a', '1000', 'Sub Bagian Umum dan Kepegawaian', '2026-02-06', '2026-02-06 06:58:08'),
+('4.b', '999', 'Sub Bagian Umum dan Kepegawaian', '2026-02-07', '2026-02-06 07:27:58'),
+('4.c', '000.5.4', 'Bidang Arsip', '2026-02-06', '2026-02-10 01:03:12'),
+('6.a ', '999', 'Bidang Perpustakaan', '2026-02-09', '2026-02-09 04:12:42'),
+('6.b', '123', 'Bidang Perpustakaan', '2026-02-05', '2026-02-10 01:54:24'),
+('7.a', '999', 'Bidang Perpustakaan', '2026-02-09', '2026-02-09 07:36:36'),
+('8.1', '900.9.9', 'Bidang Perpustakaan', '2026-02-09', '2026-02-10 01:15:45'),
+('8.a', '765', 'Sub Bagian Umum dan Kepegawaian', '2026-02-09', '2026-02-09 07:38:41'),
+('9.a', '876', 'Sub Bagian Umum dan Kepegawaian', '2026-02-09', '2026-02-10 03:26:43'),
+('9.b', '987', 'Sub Bagian Umum dan Kepegawaian', '2026-02-09', '2026-02-10 03:53:53');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengendali_sisipan_spt`
+--
+
+CREATE TABLE `pengendali_sisipan_spt` (
+  `no_urut` varchar(20) NOT NULL,
+  `klas` varchar(100) NOT NULL,
+  `plus` varchar(100) NOT NULL,
+  `tanggal_manual` date DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pengendali_sisipan_spt`
+--
+
+INSERT INTO `pengendali_sisipan_spt` (`no_urut`, `klas`, `plus`, `tanggal_manual`, `created_at`) VALUES
+('1.1', '123', 'Sub Bagian Umum dan Kepegawaian', '2026-02-10', '2026-02-10 07:14:31');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengendali_spt`
+--
+
+CREATE TABLE `pengendali_spt` (
+  `no_urut` int(2) NOT NULL,
+  `klas` varchar(100) NOT NULL,
+  `plus` varchar(100) NOT NULL,
+  `tanggal_manual` date DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pengendali_spt`
+--
+
+INSERT INTO `pengendali_spt` (`no_urut`, `klas`, `plus`, `tanggal_manual`, `created_at`) VALUES
+(1, '123', 'Sub Bagian Umum dan Kepegawaian', NULL, '2026-02-10 07:14:05');
 
 --
 -- Indexes for dumped tables
@@ -107,6 +162,18 @@ ALTER TABLE `pengendali`
 -- Indexes for table `pengendali_sisipan`
 --
 ALTER TABLE `pengendali_sisipan`
+  ADD PRIMARY KEY (`no_urut`);
+
+--
+-- Indexes for table `pengendali_sisipan_spt`
+--
+ALTER TABLE `pengendali_sisipan_spt`
+  ADD PRIMARY KEY (`no_urut`);
+
+--
+-- Indexes for table `pengendali_spt`
+--
+ALTER TABLE `pengendali_spt`
   ADD PRIMARY KEY (`no_urut`);
 
 --
